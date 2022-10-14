7757df01872cb7df2bdaddc3d9a29f2186fa7745
OB <ballesterosoriol@gmail.com>
2022-10-14 14:34:45 +0200

    1.- Akkodis Tech.Test - Basic DDBB config

---
+++ src/main/java/com/akkodis/technical/TestApplication.java
@@ -0,0 +1,19 @@
+package com.akkodis.technical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** Main class. */
+@SpringBootApplication
+public class TestApplication {
+
+	/**
+	 * Start execution method.
+	 *
+	 * @param args arguments.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
